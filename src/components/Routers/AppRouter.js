3380e13bca07174023f8f6f866dafc4708a495e0
Jermy <jeremyeparker@yahoo.com>
2021-11-14 19:46:42 -0500

    fullstack

--- src/components/Routers/AppRouter.js
+++ src/components/Routers/AppRouter.js
@@ -6,11 +6,22 @@ import {
 import NavBar from '../Navbar/NavBar';
 import Home from '../Home/Home';
 import Devs from '../Devs/Devs'
+import Lockers from '../Lockers/Lockers';
+
+
 
 const AppRouter = () => {
   return (
     <div style={{width: '100%'}}>
-    
+       <NavBar />
+      <div style={{marginTop: "75px", width: '100%'}}>
+
+        <Routes>
+          <Route path="/" exact element={<Home />}/>
+            <Route path="/developers" element={<Devs />} />
+            <Route path="/locker" element={<Lockers />} />
+        </Routes>
+      </div>
     </div>
   )
 }
